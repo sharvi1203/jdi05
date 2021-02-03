17e2b71aa9eb392aae0fbadf3c84d9ec581ee02f
kajal1811 <73377704+kajal1811@users.noreply.github.com>
2021-02-03 08:43:29 +0530

    Updated CRSService.java

--- CRS_Flipkart_Dropwizard/src/main/java/com/flipkart/rest/CRSService.java
+++ CRS_Flipkart_Dropwizard/src/main/java/com/flipkart/rest/CRSService.java
@@ -4,12 +4,25 @@ import io.dropwizard.Application;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Environment;
 
+/**
+ * @author JEDI05
+ */
 
+/**
+ * CRS Service class that extends Application class
+ */
 public class CRSService extends Application<Configuration> {
     public static void main(String[] args) throws Exception {
         new CRSService().run(args);
     }
-
+    
+    /**
+     * Run method which is called by main method of our Application
+     *
+     * @param configuration of Application class
+     * @param environment of Application class
+     * @throws Exception when exception occurs
+     */
     @Override
     public void run(Configuration configuration, Environment environment) throws Exception {
         HelloWorldResource resource = new HelloWorldResource();
